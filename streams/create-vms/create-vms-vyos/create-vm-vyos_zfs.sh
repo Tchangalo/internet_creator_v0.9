5d1428748c9bf36ef0169e0f55016d7664cb097e
Tchangalo <153798615+Tchangalo@users.noreply.github.com>
2024-12-09 05:30:56 +0100

    Update create-vm-vyos_zfs.sh

--- streams/create-vms/create-vms-vyos/create-vm-vyos_zfs.sh
+++ streams/create-vms/create-vms-vyos/create-vm-vyos_zfs.sh
@@ -18,19 +18,19 @@ then
     vmid=${provider}0${provider}0$(printf '%02d' $router)
     mgmtmac=00:24:18:A${provider}:$(printf '%02d' $router):00
 
-    #destroy
+    # Destroy
     echo -e "${C}Stopping and destroying router $vmid (if exists)${NC}"
     qm stop $vmid || true
     qm destroy $vmid || true
 
-    #create
+    # Create
     echo -e "${C}Creating router $vmid${NC}"
     qm create $vmid --name "p${provider}r${router}v" --ostype l26 --memory 1664 --balloon 1664 --cpu cputype=host --cores 4 --scsihw virtio-scsi-single --net0 virtio,bridge=vmbr1001,macaddr="${mgmtmac}"
     qm importdisk $vmid vyos-1.5.0-cloud-init-10G-qemu.qcow2 local-zfs
 	qm set $vmid --virtio0 local-zfs:vm-$vmid-disk-0
     qm set $vmid --boot order=virtio0
 
-    #add interfaces
+    # Add interfaces
 	echo -e "${C}Adding interfaces and VLAN-tags to router $vmid${NC}"
     for net in {1..4}
     do
